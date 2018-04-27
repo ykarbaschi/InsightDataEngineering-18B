0e33e3bc1b3da7f1638419548cb7655b17a8541f
ykarbaschi <ykarbaschi@yahoo.com>
2018-04-27 09:13:52 -0700

    add datatransfer scripts

---
+++ DataTransfer/findLostFiles.py
@@ -0,0 +1,26 @@
+import sys
+import quilt
+from quilt.data.examples import openimages as oi
+import numpy as np
+import subprocess
+
+# first argument is the directory we're looking for our files
+PATH = sys.argv[1]
+
+########## should change for each set
+imageIDs = oi.train.images()['ImageID']
+
+allImageIDs = sorted(np.array(imageIDs))
+
+downloadedFileNames = subprocess.check_output(['ls', PATH])
+fileNames = sorted(np.array(downloadedFileNames.splitlines()))
+
+zeroSizeFiles = subprocess.check_output(['find', PATH, '-size', '0', '-printf', '"%f\n"'])
+zeroSizeFilesNames = sorted(np.array(zeroSizeFiles.splitlines()))
+
+lostFiles = np.setdiff1d(allImageIDs, fileNames)
+
+np.append(lostFiles, zeroSizeFilesNames)
+
+np.save(PATH+'/lost.npy', lostFiles)
+#np.savetxt(PATH+'/lost.log', lostFiles, delimiter=' ', fmt="%s")
---
+++ DataTransfer/multithreadDownload.py
@@ -0,0 +1,45 @@
+from multiprocessing import Parallel
+from urllib.request import urlretrieve
+import os
+import pathlib
+import quilt
+from quilt.data.examples import openimages as oi
+import numpy as np
+
+def parallelize(data, func):
+    data_split = np.array_split(data, 10)
+    pool = Pool(10)
+    pool.map(func, data_split)
+    pool.close()
+    pool.join()
+
+def download(DF):
+    base = "/data16TB/train/images/"
+    base2 = "/data3TB/train/images/"
+    for index, rows in DF.iterrows():
+        diskStat = os.statvfs(base)
+        freeSpace = diskStat.f_bsize * diskStat.f_bfree
+        id = rows['ImageID']
+        url = rows['OriginalURL']
+        
+        if freeSpace > minDiskSpace:
+            fpath = base + id
+        else:
+            fpath = base2 + id
+
+        if (not os.path.exists(fpath)):
+            try:
+                urlretrieve(url, fpath)
+            except Exception as e:
+                print(e)
+                print('problem: {} {}'.format(url, id))
+                continue
+
+if __name__ == '__main__':
+    minDiskSpace = 1000000 #kb means ~1GB
+    
+    #pathlib.Path(base).mkdir(parents=True, exist_ok=True)
+    DF = oi.train.images._data()
+
+    parallelize(DF, download)
+    
