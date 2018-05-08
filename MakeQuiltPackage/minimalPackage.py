e903a9c8e8acf0f39f6cf853769907b024d040e0
Ubuntu <ubuntu@ip-172-31-33-162.us-east-2.compute.internal>
2018-05-08 20:09:09 +0000

    Run a real query and make panda tables and copy files

--- MakeQuiltPackage/minimalPackage.py
+++ MakeQuiltPackage/minimalPackage.py
@@ -1,50 +1,125 @@
-import quilt
-import pandas as pd
+import operator
 import os
-import shutil
 import pathlib
-
-############################################
-def GenerateREADME(readmePath):
-	with open(readmePath,'w') as myFile:
-		myFile.write('# Package Summary')
-
-def GeneratePandaTable(imageIDList, annotationDF, metadataPath):
-	#imageIDList is a python list
-	rowsResult = pd.DataFrame(data = {'ImageID' : imageIDList})
-	metadata = pd.merge(rowsResult, annotationDF)
-	metadata.to_csv(metadataPath)
-
-#########################
-
-dataFolder = './minPackage'
-imageIDList = ['b91d35b5b1e21b3a', 'b91e1150fafff17c', 'b91f356627cc68b1']
-
-GenerateREADME(dataFolder+'/README.md')
+import re
+import shutil
+import pandas as pd
+import quilt
 from quilt.data.examples import openimages as oi
-annotationDF = oi.train.annotations_human()
-GeneratePandaTable(imageIDList, annotationDF, dataFolder + '/PandaTable.csv')
 
+def GetImageIDs(folds, query):
+    #query is a regex
+    # second column should be descriptions 'label'
+    classDescriptionDF = oi.class_descriptions()
+    classDescriptionDF.columns = ['LabelName', 'LabelDescription']
+    rowsResult = classDescriptionDF[classDescriptionDF.LabelDescription.str.contains(query, regex=True,flags=re.IGNORECASE)]
+    allImageIDs = {}
+    for fold in folds:
+        ids = operator.attrgetter("{}.{}".format(fold, 'annotations_human'))(oi)().merge(rowsResult)
+        allImageIDs[fold] = ids.ImageID
+    
+    return allImageIDs
 
-if (os.path.exists(dataFolder+'/build.yml')):
-	os.remove(dataFolder+'/build.yml')
+def GenerateREADME(readmePath):
+    with open(readmePath,'w') as myFile:
+        myFile.write('# Package Summary')
 
-quilt.generate(dataFolder)
-quilt.build("ykarbaschi/minimalPackage", dataFolder+'/build.yml')
+def GeneratePandasTable(folds, allImageIDs, imageDest):
+    pandaTables = ['annotations_human', 'annotations_human_bbox', 'annotations_machine','images']
+    for fold in folds:
+        for ann in pandaTables:
+            metadata = operator.attrgetter("{}.{}".format(fold, ann))(oi)()
+            foldImageIDs = allImageIDs[fold]
+            foldMetadata = pd.merge(metadata, pd.DataFrame(data={'ImageID':foldImageIDs}))
+            metadataPath = imageDest + fold + '/' + ann + '.csv'
+            try:
+                foldMetadata.to_csv(metadataPath)
+            except Exception as e:
+                print(metadataPath + ' cannot be written')
+            
+def GetClassIDs(openImages, query):
+    classDescriptionDF = openImages.class_descriptions()
+    classDescriptionDF.columns = ['LabelName', 'LabelDescription']
+    rowsResult = classDescriptionDF[classDescriptionDF.LabelDescription.str.match(query, flags=re.IGNORECASE)]
+    return rowsResult.LabelName
 
-from quilt.data.ykarbaschi import minimalPackage
-minimalPackage._meta['trainable'] = True
-minimalPackage._meta['bbox'] = True
-minimalPackage._meta['labelName'] = 'Minimal Package'
-minimalPackage._meta['image_count'] = 3
+def get_trianable(regex, bbox=False, dry=False, frac=1):
+    """search trainable classes for matching keywords
+       only return human labeled examples with a Confidence == 1
+       regex: match against 'Description' of label
+              (hint "^(?!.*FOO).*$" negates FOO)
+       bbox: bounding box data?
+       dry: stop at label match, don't build actual test/train/validation sets
+    """    
+    trainable = oi.classes_bbox_trainable() if bbox else oi.classes_trainable()
+    descriptions = oi.class_descriptions();
+    # Label column is nameless; pandas calls it "0"
+    trainable_descriptions = pd.merge(trainable, descriptions, on='0', validate="1:1")
+    # Give columns nice names
+    trainable_descriptions.columns = ['LabelName', 'Description']
+    # Get labels that match regex
+    matches = trainable_descriptions[trainable_descriptions['Description'].str.match(regex, flags=re.IGNORECASE)]
+    # Print number of matches, matching labels
+    n_matches = len(matches)
+    #print("%s label(s) matching %s" % (n_matches, regex))
+    #print([m for m in matches['Description'].unique()])
+    if (n_matches <= 0 or dry):
+        return {}
+    # generate lists of links
+    data = {}
+    for fold in ('train', 'test', 'validation'):
+        ann = 'annotations_human_bbox' if bbox else 'annotations_human'
+        # trailing () tells quilt to turn attr into a dataframe
+        ids = operator.attrgetter("{}.{}".format(fold, ann))(oi)().sample(frac=frac, random_state=1)
+        # filter down to high confidence
+        ids = ids[ids['Confidence'] == 1]
+        matching_ids = pd.merge(matches, ids, on='LabelName', validate="1:m")
+        # image urls
+        urls = operator.attrgetter("{}.images".format(fold, ann))(oi)()
+        matching_urls = pd.merge(matching_ids, urls, on='ImageID')
+        data[fold] = matching_urls
+        print("{}: {} examples".format(fold, len(matching_urls)))
+    return data
 
-for imageID in imageIDList:
-	imageDataNode = getattr(getattr(minimalPackage, 'images'), imageID)
-	try:
-		imageDataNode._meta['bbox'] = pd.DataFrame(data=['for example bbox information'])
-	except Exception as e:
-		pass
+def CopyImages(folds, allImageIDs, sourceFolder, destFolder, logPath):
+    for fold in folds:
+        IDs = allImageIDs[fold]
+        source = sourceFolder + fold + '/images/'
+        dest = destFolder + fold + '/images/'
 
-quilt.login()
-quilt.build("ykarbaschi/minimalPackage", minimalPackage)
-quilt.push("ykarbaschi/minimalPackage", is_public=True)
+        for id in IDs:
+            full_file_name = None
+            if (os.path.isfile(source+id)):
+                full_file_name = source+id
+
+            if not full_file_name:
+                with open(logPath, 'a+') as myFile:
+                        myFile.write('this file couldnt be found {}{}\n'.format(source, id))
+                continue
+
+            if (not os.path.exists(dest+id)):
+                try:
+                    shutil.copy(full_file_name, dest)
+                except Exception as e:
+                    with open(logPath, 'a+') as myFile:
+                            myFile.write('this file can not be written be found {}{}\n'.format(dest,id))
+
+def MakeFolders(dest, folds):
+    for fold in folds:
+        path = dest + fold + '/images'
+        pathlib.Path(path).mkdir(parents=True, exist_ok=True)
+
+folds = ['test', 'train', 'validation']
+packageName = 'police_car'
+imageDest = '/data3TB/quiltPackage/'+ packageName + '/'
+
+MakeFolders(imageDest, folds)
+
+query = 'police car' 
+allImageIDs = GetImageIDs(folds, query)
+# I assumed all train data is in 16TB for now
+dataSource = '/data16TB/'
+logPath = imageDest + 'PackageBuilding.out'
+CopyImages(folds, allImageIDs, dataSource, imageDest, logPath)
+
+GeneratePandasTable(folds, allImageIDs, imageDest)
